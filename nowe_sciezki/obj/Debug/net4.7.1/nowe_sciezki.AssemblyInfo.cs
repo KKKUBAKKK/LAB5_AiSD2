d926e923bf7c4785423c2acdc884d146d1190e92
KKKUBAKKK <kuba.kindracki@gmail.com>
2024-03-28 12:28:53 +0100

    All I did on labs, Stage 1 working, but not reconstructing path.

--- nowe_sciezki/obj/Debug/net4.7.1/nowe_sciezki.AssemblyInfo.cs
+++ nowe_sciezki/obj/Debug/net4.7.1/nowe_sciezki.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nowe_sciezki")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab5952cb717876bb4ad1c232cf9ac324d16a6dee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("nowe_sciezki")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nowe_sciezki")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
